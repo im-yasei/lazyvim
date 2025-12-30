a5acc820ae08f9516333b6abe0082e34e584f9fe
yasei <arazdakov@gmail.com>
2025-12-30 21:12:13 +0300

    fix: restore Verilog/SystemVerilog filetype detection

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Setting the filetype for Verilog
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.v" },
+  command = "set filetype=verilog",
+})
+
+-- Setting the filetype for SystemVerilog
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.sv" },
+  command = "set filetype=systemverilog",
+})
